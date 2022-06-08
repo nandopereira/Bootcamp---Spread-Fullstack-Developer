9dad769e823f6812a8222da4c7338b95f2e839db
Fernando Pereira <fernando.pmoraes17@gmail.com>
2022-06-08 11:41:30 -0300

    Sistema de Estacionamento com TypeScript(completo)

---
+++ Trabalhando com Módulos em JavaScript/atividade/functions.js
@@ -0,0 +1,13 @@
+function showAge(name, age) {
+  return `${name} is ${age} years old`
+}
+
+function showCity(city) {
+  return `From ${city}`
+}
+
+function showHobby(hobby) {
+  return hobby
+}
+
+export { showAge, showCity, showHobby }
---
+++ Trabalhando com Módulos em JavaScript/atividade/main.js
@@ -0,0 +1,5 @@
+import { showAge, showCity, showHobby } from './functions.js'
+
+console.log(showAge('Mônica', 24))
+console.log(showCity('São Paulo'))
+console.log(showHobby('Watching Soccer Games'))
